680cdbb4388147cf80058bbb8f381769749e91bd
david-anderson-curebase <david.anderson@curebase.com>
2022-09-02 15:51:32 -0700

    some changes made to the functions, imported the FS module so we can dump the results into new files, easily packaged for delivery to Tugboat

--- GetReports.js
+++ GetReports.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
-let bearerToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdXRoZW50aWNhdGVkLWFwcCI6IkdFTkVSSUMiLCJhdXRoZW50aWNhdGlvbi10eXBlIjoiSlNTIiwiZ3JvdXBzIjpbXSwic3ViamVjdC10eXBlIjoiSlNTX1VTRVJfSUQiLCJ0b2tlbi11dWlkIjoiZDNjYjZkYzMtMGU4ZC00YjIyLWEyZWYtZGQ2Y2Q1NDFhNDI0IiwibGRhcC1zZXJ2ZXItaWQiOi0xLCJzdWIiOiIxNSIsImV4cCI6MTY2MjE1ODcwMH0.rxS3ThenOBQVzrBcLqnFV76AyPyH8n7FJHyTTE17ct8';
+let bearerToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdXRoZW50aWNhdGVkLWFwcCI6IkdFTkVSSUMiLCJhdXRoZW50aWNhdGlvbi10eXBlIjoiSlNTIiwiZ3JvdXBzIjpbXSwic3ViamVjdC10eXBlIjoiSlNTX1VTRVJfSUQiLCJ0b2tlbi11dWlkIjoiNzA4M2ZhMmItM2Q3OC00OGViLWIzN2ItYzk5MTBkNzg1YTUzIiwibGRhcC1zZXJ2ZXItaWQiOi0xLCJzdWIiOiIxNSIsImV4cCI6MTY2MjE2MDYxNn0.owvoxL5lGwYo5phh2koMqRlE2OE5xPhNJbwnB-IbCKw';
 
-const url = 'https://curebase.jamfcloud.com/JSSResource/advancedcomputersearches?='
+const url = 'https://curebase.jamfcloud.com/JSSResource/advancedcomputersearches'
 const headers = {
     method: 'GET',
     headers: {
