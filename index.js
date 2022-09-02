e740c534dbed68ef8431696966b3c538a2dc358b
david-anderson-curebase <david.anderson@curebase.com>
2022-09-02 13:37:21 -0700

    refactored with a dotenv deployed (and applied to gititgnore)

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+require('dotenv').config();
+console.log(`Authorization URL: ${process.env.URL}`)
+console.log(`Basic Authorization Account: ${process.env.ACCOUNT}`)
+console.log(`Basic Authorization Credentials: ${process.env.CREDENTIAL}`)
 
-const credentials = btoa('TugboatCaptain:PbuEqLYJjV30ITlfYawa');
-var token;
 
-async function getToken(url = '') {
+/*const credentials = btoa('TugboatCaptain:PbuEqLYJjV30ITlfYawa');
+const url = 'https://curebase.jamfcloud.com/api/v1/auth/token'
+
+let bearerToken = [];
+
+async function getToken(url) {
     const response = await fetch(url, {
         method: 'POST',
         headers: {
@@ -12,13 +19,12 @@ async function getToken(url = '') {
         redirect: 'follow'
     })
     .then(res => res.json())
-    .then(data => token = data)
+    .then(data => bearerToken.push(data))
+    .then(data => console.log(data))
     .catch(error => console.log('error', error))
 }
 
-async function returnToken(url) {
-    let token = await getToken(url);
-}
 
-getToken('https://curebase.jamfcloud.com/api/v1/auth/token')
-console.log(token);
+
+getToken(url)
+console.log(bearerToken);*/
