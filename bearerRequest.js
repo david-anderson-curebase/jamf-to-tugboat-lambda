9759596f19f0f2c1e0f4499136bc92c2bfb7c0af
david-anderson-curebase <david.anderson@curebase.com>
2022-09-06 13:50:37 -0700

    getToken refactored, variables (temporarily) moved to index.js). getToken imported into and working through index.js call

--- bearerRequest.js
+++ bearerRequest.js
@@ -1,35 +1,10 @@
-// .ENV contains our secure credential information as well as the basic URL for our bearer token call
-import * as dotenv from 'dotenv';
-dotenv.config();
-import * as fs from 'fs';
-
-//const getReport = require('./GetReport.js');
-// formats our credentials as a Base64 string for use in the API call
-const credentials = btoa(`${process.env.ACCOUNT}:${process.env.CREDENTIAL}`);
-const url = process.env.TOKEN_URL;
-const headers = {
-    method: 'POST',
-    headers: {
-        'accept': 'application/json',
-        'Authorization': `Basic ${credentials}`
-    },
-    redirect: 'follow'
-};
-
 function getToken(url, headers) {
     let token;
     fetch(url, headers)
     .then(res => res.json())
     .then(data => data.token)
-    //.then(data => cb(data))
+    .then(token => console.log(token))
     .catch(error => console.log('error', error))
 }
 
-/*let token = fs.readFile('bearer_token', "utf8", (err, data) => {
-    console.log(data);
-});*/
-
-getToken();
-//export default getToken;
-/*exports.payload = getToken(url, headers);
-exports.message = 'oi oi oi'*/
+export { getToken };
--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-import { welcome, helloYou } from "./Scratch.js";
-helloYou(welcome);
+import dotenv from 'dotenv';
+dotenv.config();
+import { getToken } from "./bearerRequest.js";
+
+const credentials = btoa(`${process.env.ACCOUNT}:${process.env.CREDENTIAL}`);
+const url = process.env.TOKEN_URL;
+const headers = {
+    method: 'POST',
+    headers: {
+        'accept': 'application/json',
+        'Authorization': `Basic ${credentials}`
+    },
+    redirect: 'follow'
+};
+
+console.log(getToken(url, headers));
