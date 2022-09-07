2ef318d2a88fad2950a6968b40d0ae4d8bcc5342
david-anderson-curebase <david.anderson@curebase.com>
2022-09-07 14:53:12 -0700

    followup

--- GetTimestamp.js
+++ GetTimestamp.js
@@ -2,12 +2,11 @@ const getTimestamp = () => {
     let time = new Date;
     let date = String(time.getDate());
     let month = String(time.getMonth());
-    function convertDigits(time) {
-        (date.length > 1) ? date = `${date}` : date = `0${date}`;
-        (month.length > 1) ? month = `${month}` : month = `0${month}`;
-      }
-    convertDigits();
-    console.log(`${time.getFullYear()}-${month}-${date}`)
+    (date.length > 1) ? date = `${date}` : date = `0${date}`;
+    (month.length > 1) ? month = `${month}` : month = `0${month}`;
+    console.log(`${time.getFullYear()}-${month}-${date}`);
 }
+
+getTimestamp()
 
 export { getTimestamp };
