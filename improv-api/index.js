f5d2feb6f41ea9695f04bc45305c810d47fc0a84
david-anderson-curebase <david.anderson@curebase.com>
2022-10-04 11:13:53 -0700

    another datadog test

--- improv-api/index.js
+++ improv-api/index.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 import { bearerHeaders, bearerUrl } from './headers.js';
 
-const call = (url, headers) => {fetch(url, headers).then(res => res.json()).then(res => console.log(res))}
+const tokenCall = (url, headers, cb) => {fetch(url, headers).then(res => res.json()).then(res => console.log(res))}
 
 call(bearerUrl, bearerHeaders)
